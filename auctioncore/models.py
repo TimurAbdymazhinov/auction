73d33f961ca96fbe0e1fd2d63714b9669744b324
Timur Abdymazhinov <abdymazhinov.timur@gmail.com>
2020-04-10 02:00:32 +0600

    add models

--- auctioncore/models.py
+++ auctioncore/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
+from category.models import Category
 
-# Create your models here.
+
+class Auction(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='auctions')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='auctions')
+
+    is_client = models.BooleanField(default=False)
+    is_auction_owner = models.BooleanField(default=False)
+    title = models
+    country = models.CharField(max_length=200, verbose_name='Страна', null=True, blank=True)
+    city = models.CharField(max_length=200, verbose_name='Город', null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return
--- category/models.py
+++ category/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=200, null=True, blank=True)
+    order = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    is_accepted = models.BooleanField(default=True)
+
+    owner = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='subcategories')
+
+    def __str__(self):
+        return self.title
