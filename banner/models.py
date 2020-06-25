a1ad18efc433d02083809474760aea1106f4fed3
Timur Abdymazhinov <abdymazhinov.timur@gmail.com>
2020-06-25 21:07:00 +0600

    add link from banner to link, fixed bugs

--- banner/models.py
+++ banner/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
-
+from auctioncore.models import Auction
 
 class Banner(models.Model):
     name = models.CharField(max_length=50, null=True, blank=True)
     is_active = models.BooleanField(default=True, blank=True, null=True)
     image = models.ImageField(upload_to='banner', null=True, blank=True)
-
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE,  null=True, blank=True)
     def __str__(self):
         return self.name
 
